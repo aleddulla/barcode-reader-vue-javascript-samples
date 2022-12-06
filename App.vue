f4cfa9ae427e5591480ea10e9896630b122d62b2
Srikanth Reddy A <sreekanthreddy.com@gmail.com>
2022-12-06 13:29:27 +0400

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
